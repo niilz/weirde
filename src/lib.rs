b84536db7904597cb8d9d40a37d7e99a89b6341a
niilz <niilz@outlook.de>
2024-01-05 17:12:46 +0100

    Move WireType-detection to a function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
 
+const CONTINUE_MASK: u8 = 0b1000_0000;
+const WIRE_TYPE_MASK: u8 = 0b0000_0111;
+const FIELD_NUM_MASK: u8 = 0b0111;
+
 pub trait Proto {
     fn serialize(&self) -> &[u8];
 }
@@ -19,19 +23,25 @@ enum WireType {
 fn deserialize(bin: &[u8]) -> HashMap<u8, WireType> {
     let bin: Vec<_> = bin.iter().skip_while(|b| *b == &0u8).collect();
     let key = &bin[0];
-    let continue_mask = 0b1000_0000;
-    let is_continue = *key & continue_mask;
-    eprintln!("is_continue: {is_continue}");
-    let wire_type_mask = 0b0000_0111;
-    let wire_type = *key & wire_type_mask;
-    eprintln!("wire_type: {wire_type}");
-    let field_num_mask = 0b0111;
-    let field_num = *key >> 3 & field_num_mask;
-    eprintln!("field_num: {field_num}");
+    let wire_type = map_to_wire_type(**key, 42);
     let mut msg = HashMap::new();
     let wire_s = WireType::Len("Foo".to_string());
     msg.insert(1u8, wire_s);
     msg
+}
+
+fn map_to_wire_type(key: u8, value: u64) -> WireType {
+    let is_continue = key & CONTINUE_MASK;
+    eprintln!("is_continue: {is_continue}");
+    let wire_type = key & WIRE_TYPE_MASK;
+    eprintln!("wire_type: {wire_type}");
+    let field_num = key >> 3 & FIELD_NUM_MASK;
+    eprintln!("field_num: {field_num}");
+    match field_num {
+        0 => WireType::Varint(value),
+        2 => WireType::Len("TODO".to_string()),
+        _ => panic!("Unsupported Wire-Type"),
+    }
 }
 
 #[cfg(test)]
@@ -49,7 +59,6 @@ mod test {
     #[test]
     fn basic_bin_to_msg() {
         let bin: u64 = 0x089601;
-        let bin: u64 = 0x0a0141;
         let msg = deserialize(&bin.to_be_bytes());
         assert_eq!(WireType::Varint(150), *msg.get(&1).unwrap())
     }
