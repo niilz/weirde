479dcc01e9189a193f97eed3824c4dff09ba3741
niilz <niilz@outlook.de>
2024-01-07 14:18:35 +0100

    Generalize a little more to prepare Len-Handling

--- src/serialize.rs
+++ src/serialize.rs
@@ -5,12 +5,36 @@ use crate::{
 };
 
 pub trait Proto {
+    fn proto_msg(&self) -> Vec<u8>;
+}
+
+struct SimpleMessage {
+    a: u64,
+    b: String,
+}
+
+impl Proto for SimpleMessage {
+    fn proto_msg(&self) -> Vec<u8> {
+        let a = self.a.serialize(1);
+        let b = self.b.serialize(2);
+        [a, b].concat()
+    }
+}
+
+pub trait WireFormat {
     fn serialize(&self, field: u8) -> Vec<u8>;
 }
 
-impl Proto for u64 {
+impl WireFormat for u64 {
     fn serialize(&self, field: u8) -> Vec<u8> {
         let wire_type = WireType::Varint(*self);
+        wire_type.serialize(field)
+    }
+}
+
+impl WireFormat for String {
+    fn serialize(&self, field: u8) -> Vec<u8> {
+        let wire_type = WireType::Len(self.to_string());
         wire_type.serialize(field)
     }
 }
@@ -20,23 +44,24 @@ impl Proto for u64 {
 //const FIELD_NUM_MASK: u8 = 0b0111;
 //const U64_MAX_LEN: usize = 16;
 
-impl Proto for WireType {
+impl WireFormat for WireType {
     fn serialize(&self, field: u8) -> Vec<u8> {
-        let (key, value) = match self {
+        let key = format!("0{:04b}000", field);
+        let key = u64::from_str_radix(&key, 2).expect("key binary-conversion failed");
+        let key = key.to_be_bytes().to_vec();
+        let value = match self {
             WireType::Varint(v) => {
                 // 0 => no continuation bit
                 //  xxxx => the field number
                 //      000 => wire_type: 0 == VARINT
-                let key = format!("0{:04b}000", field);
-                let key = u64::from_str_radix(&key, 2).expect("key binary-conversion failed");
                 let value = format!("{v:b}");
                 let value = u64::from_str_radix(&value, 2).expect("value binary-conversion failed");
                 println!("{v}");
-                (key, value)
+                value.to_be_bytes().to_vec()
             }
-            WireType::Len(_) => todo!(),
+            WireType::Len(s) => s.as_bytes().to_vec(),
         };
-        [key.to_be_bytes(), value.to_be_bytes()]
+        [key, value]
             .iter()
             .flat_map(|bytes| {
                 bytes
@@ -53,10 +78,8 @@ impl Proto for WireType {
 mod test {
     use super::*;
 
-    const BASIC_MSG: &str = "1: 42";
-
     #[test]
-    fn basic_msg_to_bin() {
+    fn basic_num_to_bin() {
         let ser = 42.serialize(1);
         // 082a
         assert_eq!(vec![8, 42], ser);
